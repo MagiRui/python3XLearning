d428dbcb228317d353cae8e6d6f44bf851b373a4
admin <jr1_2007@163.com>
2018-09-15 09:49:33 +0800

    new testing

---
+++ p2c5/thread-count-wait2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#!coding:utf8
+#author:MagiRui
+
+
+import _thread as thread
+stdoutmutex = thread.allocate_lock()
+exitmutexes = [False] * 10
+
+def counter(myId, count):
+
+    for i in range(count):
+
+        stdoutmutex.acquire()
+        print("[%s] => %s" %(myId, i))
+        stdoutmutex.release()
+
+    exitmutexes[myId] = True
+
+for i in range(10):
+
+    thread.start_new_thread(counter, (i, 100))
+
+while False in exitmutexes: pass
+
+print("Main thread exiting.")
